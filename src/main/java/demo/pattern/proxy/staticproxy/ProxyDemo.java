5bd8690a83b522e2ac8d33dfe91805016aecefd4
wander <xiaoyaole3@163.com>
2021-12-28 17:51:27 +0800

    1. 增加静态代理模式的注释

--- src/main/java/demo/pattern/proxy/staticproxy/ProxyDemo.java
+++ src/main/java/demo/pattern/proxy/staticproxy/ProxyDemo.java
@@ -3,6 +3,7 @@ package demo.pattern.proxy.staticproxy;
 import demo.pattern.proxy.staticproxy.impl.RealPayToC;
 import demo.pattern.proxy.staticproxy.impl.ToCPaymentImpl;
 
+// 静态代理模式在编译时就将代码写入到对应的对象中
 public class ProxyDemo {
     public static void main(String[] args) {
         ToCPayment toCPayment = new RealPayToC(new ToCPaymentImpl());
