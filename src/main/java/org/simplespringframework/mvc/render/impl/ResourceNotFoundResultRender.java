7b39111ddbbf00303791c0ab8fe92ec07687311f
wander <xiaoyaole3@163.com>
2022-01-07 10:23:41 +0800

    1. ResourceNotFoundResultRender的实现 2. 在tomcat7 plugin的pom文件中增加使用说明

--- src/main/java/org/simplespringframework/mvc/render/impl/ResourceNotFoundResultRender.java
+++ src/main/java/org/simplespringframework/mvc/render/impl/ResourceNotFoundResultRender.java
@@ -3,16 +3,25 @@ package org.simplespringframework.mvc.render.impl;
 import org.simplespringframework.mvc.RequestProcessorChain;
 import org.simplespringframework.mvc.render.ResultRender;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 资源找不到时使用的渲染器
  */
 public class ResourceNotFoundResultRender implements ResultRender {
-    public ResourceNotFoundResultRender(String method, String requestPath) {
 
+    private String httpMethod;
+    private String httpPath;
+
+
+    public ResourceNotFoundResultRender(String method, String requestPath) {
+        this.httpMethod = method;
+        this.httpPath = requestPath;
     }
 
     @Override
     public void render(RequestProcessorChain requestProcessorChain) throws Exception {
-
+        requestProcessorChain.getResponse().sendError(HttpServletResponse.SC_NOT_FOUND,
+                "Can not find resource, httpPath = [" + httpPath + "], httpMethod = [" + httpMethod + "]");
     }
 }
